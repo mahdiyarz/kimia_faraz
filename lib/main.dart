055b384acaa6864f1ab17f5a6b619134ddc883af
mahdiyarz <m.arbabzi@gmail.com>
2022-04-14 17:56:04 +0430

    Create first look of theme

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,21 @@ import './screens/home.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: const Home(),
+      home: const Directionality(
+        textDirection: TextDirection.rtl,
+        child: Home(),
+      ),
       theme: ThemeData(
-        primaryColor: const Color.fromARGB(255, 212, 224, 155),
+        canvasColor: Color.fromARGB(255, 246, 244, 210),
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color.fromARGB(255, 212, 224, 155),
+          onPrimary: const Color.fromARGB(255, 0, 0, 0),
+          secondary: const Color.fromARGB(255, 241, 156, 121),
+          onSecondary: const Color.fromARGB(255, 0, 0, 0),
+          error: const Color.fromARGB(255, 164, 74, 63),
+          onError: const Color.fromARGB(255, 255, 255, 255),
+          onPrimaryContainer: const Color.fromARGB(255, 203, 223, 189),
+        ),
       ),
     ),
   );
