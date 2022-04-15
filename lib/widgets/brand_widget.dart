30ef28a03f9eed5ddc5249c86cb8d4d51dc8d65f
mahdiyarz <m.arbabzi@gmail.com>
2022-04-15 21:50:51 +0430

    Set and Create some pics of hole a BRAND

--- lib/widgets/brand_widget.dart
+++ lib/widgets/brand_widget.dart
@@ -8,43 +8,68 @@ class BrandWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
-        return InkWell(
-          onTap: () {},
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: 110,
-            margin:
-                const EdgeInsets.only(right: 10, left: 10, bottom: 5, top: 5),
-            padding: const EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(8),
-                topLeft: Radius.circular(50),
-                topRight: Radius.circular(8),
-              ),
-              color: Theme.of(context).colorScheme.primaryContainer,
-            ),
-            child: Row(
+        return Container(
+          width: MediaQuery.of(context).size.width,
+          height: 160,
+          child: InkWell(
+            onTap: () {},
+            child: Stack(
               children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      MY_DATA[index].brandName,
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+                Positioned(
+                  top: 55,
+                  child: Container(
+                    width: MediaQuery.of(context).size.width / 1.3,
+                    height: 100,
+                    margin: const EdgeInsets.only(right: 25),
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.35),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset: Offset(1, 3),
+                        )
+                      ],
+                      borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(20),
+                        bottomRight: Radius.circular(8),
+                        topLeft: Radius.circular(50),
+                        topRight: Radius.circular(8),
                       ),
+                      color: Theme.of(context).colorScheme.primaryContainer,
                     ),
-                    Text(
-                      MY_DATA[index].brandLatinName.toString(),
-                      style: const TextStyle(
-                        color: Colors.black38,
-                      ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          MY_DATA[index].brandName,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          MY_DATA[index].brandLatinName.toString(),
+                          style: const TextStyle(
+                            color: Colors.white54,
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
+                  ),
                 ),
+                Positioned(
+                  right: MediaQuery.of(context).size.width / 2.3,
+                  child: SizedBox(
+                    width: 150,
+                    height: 160,
+                    child: Image.asset(
+                      MY_DATA[index].brandImage,
+                    ),
+                  ),
+                )
               ],
             ),
           ),
