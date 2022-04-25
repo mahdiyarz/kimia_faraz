3e1df38733eabb81b1b8515ac2ecb5bc34b5fb87
mahdiyarz <m.arbabzi@gmail.com>
2022-04-26 02:09:37 +0430

    Connect home page to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'screens/homePage.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: const Directionality(
         textDirection: TextDirection.rtl,
-        child: Home(),
+        child: HomePage(),
       ),
       theme: ThemeData(
-        canvasColor: Color.fromARGB(255, 246, 244, 210),
+        canvasColor: const Color.fromARGB(255, 101, 93, 138),
         colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: const Color.fromARGB(255, 212, 224, 155),
-          onPrimary: const Color.fromARGB(255, 0, 0, 0),
-          secondary: const Color.fromARGB(255, 241, 156, 121),
-          onSecondary: const Color.fromARGB(255, 0, 0, 0),
+          primary: const Color.fromARGB(255, 120, 151, 171),
+          onPrimary: const Color.fromARGB(255, 255, 255, 255),
+          secondary: const Color.fromARGB(255, 216, 133, 163),
+          onSecondary: const Color.fromARGB(255, 255, 255, 255),
           error: const Color.fromARGB(255, 164, 74, 63),
           onError: const Color.fromARGB(255, 255, 255, 255),
-          primaryContainer: const Color.fromARGB(255, 203, 223, 189),
+          primaryContainer: const Color.fromARGB(255, 253, 206, 185),
         ),
       ),
     ),
